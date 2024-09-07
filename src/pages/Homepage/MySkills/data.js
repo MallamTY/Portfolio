2e5fee948824c962acff92418bb036d0f14b8909
mallamty <tnsosanya@gmail.com>
2024-09-07 10:29:07 +0100

    Added emailjs and exchange collective project

---
+++ src/pages/Homepage/MySkills/data.js
@@ -0,0 +1,80 @@
+export const skillsData = [
+  {
+    setsName: 'Backend',
+    skillSet: [
+      {
+        icon: 'rest.png',
+        name: 'REST'
+      },
+      {
+        icon: 'graphql.png',
+        name: 'GraphQL'
+      },
+      {
+        icon: 'js.png',
+        name: 'Javascript'
+      },
+      {
+        icon: 'nodejs.png',
+        name: 'Nodejs'
+      },
+      {
+        icon: 'firebase.webp',
+        name: 'Firebase'
+      },
+      {
+        icon: 'python.svg',
+        name: 'Python'
+      }
+    ]
+  },
+  {
+    setsName: 'Database',
+    skillSet: [
+      {
+        icon: 'mongodb.png',
+        name: 'MongoDB'
+      },
+      {
+        icon: 'postgresql.png',
+        name: 'PostgreSQL'
+      },
+      {
+        icon: 'mysql.png',
+        name: 'MySQL'
+      }
+    ]
+  },
+
+  {
+    setsName: 'DevOps',
+    skillSet: [
+      {
+        icon: 'kubernetes.png',
+        name: 'Kubernetes'
+      },
+      {
+        icon: 'docker.png',
+        name: 'Docker'
+      },
+      {
+        icon: 'terraform.png',
+        name: 'Terraform'
+      },
+      {
+        icon: 'aws.png',
+        name: 'AWS'
+      }
+    ]
+  },
+
+  {
+    setsName: 'Tools',
+    skillSet: [
+      {
+        icon: 'github.png',
+        name: 'GitHub'
+      },
+    ]
+  }
+] 
---
+++ src/pages/Projects/data.js
@@ -0,0 +1,50 @@
+export const projectData = [
+  { 
+    name: 'Pinoy Used Cars', 
+    img: 'pinoy.png', 
+    liveLink: 'https://pinoyusedcars.com/en', 
+    description: 'Find your next car with PinoyUsedCars! Our app provides a seamless and user-friendly experience for browsing a wide range of high-quality pre-owned vehicles.'
+  },
+  { 
+    name: 'Cars UAE MARKET', 
+    img: 'carsuaemarket.png', 
+    liveLink: 'https://www.carsuaemarket.com/en', 
+    description: 'Welcome to Cars UAE Market, your ultimate destination for buying and selling cars in the UAE. Our app is designed to provide a seamless and user-friendly experience, ensuring you find the perfect vehicle or sell your car with ease.'
+  },
+  { 
+    name: 'Exchange Collective Inc.', 
+    img: 'ExchangeCollective.png', 
+    liveLink: 'https://exchangecollective.com/', 
+    description: 'The original vision, “to give small-to-medium size, independent retail stores a chance to play with the big boys, to thrive and grow in online marketplaces,” still drives every effort at EXC. We believe people behind those businesses, as well as their customers, are the driving forces that help our local communities to flourish. Exchange Collective is dedicated to their success!'
+  },
+  { 
+    name: 'Damage Auto', 
+    img: 'damageautoweb.png', 
+    liveLink: 'https://www.damageauto.com/en', 
+    description: 'Your ultimate car marketplace! Connect with genuine buyers and sellers. From vintage classics to modern marvels, find or list your dream ride. Secure, simple, and swift. Drive your passion.'
+  },
+]
+
+export const mobileProjectData = [
+  { 
+    name: 'Pinoy Used Cars', 
+    img: 'pinoymobile.png', 
+    iosLink: 'https://apps.apple.com/us/app/pinoy-used-cars/id6504991858', 
+    androidLink: 'https://play.google.com/store/apps/details?id=com.pinoyusedcars.pinoyusedcars', 
+    description: 'Find your next car with PinoyUsedCars! Our app provides a seamless and user-friendly experience for browsing a wide range of high-quality pre-owned vehicles.'
+  },
+  { 
+    name: 'Cars UAE MARKET', 
+    img: 'carsuaemarketmobile.png', 
+    iosLink: 'https://apps.apple.com/us/app/cars-uae-market/id6504081367', 
+    androidLink: 'https://play.google.com/store/apps/details?id=com.carsuaemarket.pinoyusedcars', 
+    description: 'Welcome to Cars UAE Market, your ultimate destination for buying and selling cars in the UAE. Our app is designed to provide a seamless and user-friendly experience, ensuring you find the perfect vehicle or sell your car with ease.'
+  },
+  { 
+    name: 'Damage auto', 
+    img: 'damageauto.png', 
+    iosLink: 'https://apps.apple.com/dk/app/damage-auto/id6476107712', 
+    androidLink: 'https://play.google.com/store/apps/details?id=com.damageauto.flipanycommercial&hl=en&gl=US', 
+    description: 'Your ultimate car marketplace! Connect with genuine buyers and sellers. From vintage classics to modern marvels, find or list your dream ride. Secure, simple, and swift. Drive your passion.'
+  },
+]
